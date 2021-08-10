45d250651065ee6b77b5d9a84cd4c8b2ca446ea6
Stephen Montgomery <stephen.montgomery@sievo.com>
2021-08-10 16:55:15 +0300

    Logging Behaviour

--- sjm.mediatr/Startup.cs
+++ sjm.mediatr/Startup.cs
@@ -1,18 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using sjm.mediatr.Behaviours;
 
 namespace sjm.mediatr
 {
@@ -31,6 +24,7 @@ namespace sjm.mediatr
             //services.AddMediatR(Assembly.GetExecutingAssembly());
             services.AddMediatR(typeof(Startup));
             services.AddSingleton<FakeDataStore>();
+            services.AddSingleton(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
